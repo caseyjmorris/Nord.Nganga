0939704ec977ca252136a0a2a39f1efff6f0d6ae
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-07-08 13:41:09 -0400

    added StEngine project

---
+++ Nord.AngularUiGen.StEngine.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nord.AngularUiGen.StEngine.Test
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      var t = TemplateFactory.GetTemplate(TemplateFactory.Context.View, "t1");
+      t.Add("model",new { lastName ="Lloyd", sex="male"} );
+      var s = t.Render();
+      Console.WriteLine(s);
+    }
+  }
+}
---
+++ Nord.AngularUiGen.StEngine/TemplateFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.StringTemplate;
+
+namespace Nord.AngularUiGen.StEngine
+{
+  public class TemplateFactory
+  {
+
+    public enum Context { View, Controller, Service }
+    public static Template GetTemplate(Context context, string name)
+    {
+      var fileName = string.Format(@"{0}/templates/{1}/default.stg", Environment.CurrentDirectory, context);
+      Console.WriteLine(fileName +":");
+      Console.Write(File.ReadAllText(fileName));
+      Console.WriteLine("---");
+      TemplateGroup group = new TemplateGroupFile(fileName);
+      var template = group.GetInstanceOf(name);
+      return template;
+    }
+  }
+}
