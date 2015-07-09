d9780acc5f7d4214c25d4a411895f741e3480052
Casey J. Morris <caseyjmorris@gmail.com>
2015-07-09 17:24:19 -0400

    Hey, the binding works.

--- Nord.Nganga.ClientPackage/Scripts/ui/directives/nord.multiple.complex.editor.js
+++ Nord.Nganga.ClientPackage/Scripts/ui/directives/nord.multiple.complex.editor.js
@@ -11,52 +11,58 @@ ngangaUi.directive('nordMultipleComplexEditor', ['$parse', function($parse)
         additionalButtons: '@',
         fieldDefinitions: '@'
       },
-      post: function(scope, element)
+      compile: function(element, attributes)
         {
-          alert(JSON.stringify(scope.fieldDefinitions));
-          scope.activeItem = {};
-          scope.activeItemIndex = null;
-          scope.additionalButtonsValues = $parse(element.additionalButtons);
-          scope.fieldDefinitionValues = $parse(element.fieldDefinitions);
-          element.parentObject[element.collectionName] = element.parentObject[element.collectionName] || [];
+          return {
+            post: function(scope, element)
+              {
+                window.parser = $parse;
+                scope.activeItem = {};
+                scope.activeItemIndex = null;
+                //scope.additionalButtonsValues = $parse(element.additionalButtons).call();
+                scope.fieldDefinitionValues = $parse(scope.fieldDefinitions)();
+                scope.parentObject[scope.collectionName] = scope.parentObject[scope.collectionName] || [];
 
-          scope.collection = element.parentObject[element.collectionName];
+                scope.collection = scope.parentObject[scope.collectionName];
 
-          scope.addItem = function()
-            {
-              scope.activeItem = {};
-              scope.activeItemIndex = null;
-              //show control
-            };
+                scope.addItem = function()
+                  {
+                    scope.activeItem = {};
+                    scope.activeItemIndex = null;
+                    //show control
+                  };
 
-          scope.editItemAt = function(index)
-            {
-              scope.activeItem = {};
-              scope.activeItemIndex = index;
-              $.extend(scope.activeItem, scope.collection[index]);
-              //show child form
-            };
+                scope.editItemAt = function(index)
+                  {
+                    scope.activeItem = {};
+                    scope.activeItemIndex = index;
+                    $.extend(scope.activeItem, scope.collection[index]);
+                    //show child form
+                  };
 
-          scope.deleteItemAt = function(index)
-            {
-              scope.collection.splice(index, 1);
-              //set parent form dirty
-            };
+                scope.deleteItemAt = function(index)
+                  {
+                    scope.collection.splice(index, 1);
+                    //set parent form dirty
+                  };
 
-          scope.applyActiveItemChanges = function()
-            {
-              if (scope.activeItemIndex === null)
-                {
-                  scope.collection.push(scope.activeItem);
-                }
-              else
-                {
-                  scope.collection[scope.activeItemIndex] = scope.activeItem;
-                }
-              //close child form
-              //set child form pristine
-              //set parent form dirty
-            }
+                scope.applyActiveItemChanges = function()
+                  {
+                    if (scope.activeItemIndex === null)
+                      {
+                        scope.collection.push(scope.activeItem);
+                      }
+                    else
+                      {
+                        scope.collection[scope.activeItemIndex] = scope.activeItem;
+                      }
+                    //close child form
+                    //set child form pristine
+                    //set parent form dirty
+                  }
+              }
+          }
         }
+
     }
   }]);
