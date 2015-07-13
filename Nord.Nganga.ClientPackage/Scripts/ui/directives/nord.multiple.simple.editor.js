45bef46bdb3a3153e87826ef58bea84e93c53970
Casey J. Morris <caseyjmorris@gmail.com>
2015-07-13 17:12:52 -0400

    This has some alignment issues in the view but it's functional so I think we can more or less go forward with it...

--- Nord.Nganga.ClientPackage/Scripts/ui/directives/nord.multiple.simple.editor.js
+++ Nord.Nganga.ClientPackage/Scripts/ui/directives/nord.multiple.simple.editor.js
@@ -1,17 +1,35 @@
 (function()
   {
     var template =
-      '<div class="col-md-{{twelfths}}"><label ng-class="{\'required-field-label\': isRequired}">' +
-      '{{fieldLabelText}}</label><div class="panel panel-default">  <ul class="list-group"> ' +
+      '<div class="simple-editor-enclosing">' +
+      '<label ng-class="{\'required-field-label\': isRequired}">' +
+      '{{fieldLabelText}}</label>' +
+      '<div class="panel panel-default"> ' +
+      '<ul class="list-group"> ' +
       ' <li class="list-group-item"> ' +
       ' <div class="row"> ' +
-      ' <div class="col-md-10 pseudotransclude">  </div>  ' +
+      ' <div class="col-md-10 pseudotransclude"> ' +
+      ' </div>  ' +
       '<div class="col-md-2">  ' +
-      '<button type="button" class="btn btn-danger drop-item-button">' +
-      '<span class="glyphicon glyphicon-remove"></span> Remove</button>' +
+      '<button type="button" class="btn btn-danger drop-item-button pull-right">' +
+      '<span class="glyphicon glyphicon-remove">' +
+      '</span> ' +
+      'Remove' +
+      '</button>' +
       '</div>' +
       '</div>' +
-      '</li></ul></div></div>';
+      '</li>' +
+      '<li>' +
+      '<nord-row>' +
+      '<button type="button" class="btn btn-default push-collection-simple-button">' +
+      '<span class="glyphicon glypicon-plus"></span>' +
+      'Add' +
+      '</button>' +
+      '</nord-row>' +
+      '</li>' +
+      '</ul>' +
+      '</div>' +
+      '</div>';
 
     function getNgRepeatStatement(childFieldName, parentObj, childCollectionName)
       {
@@ -21,6 +39,11 @@
     function getDeleteFunction(parentObj, childCollectionName)
       {
         return parentObj + '.' + childCollectionName + '.splice($index, 1)';
+      }
+
+    function getAddFunction(parentObj, childCollectionName)
+      {
+        return parentObj + '.' + childCollectionName + '.push({});'
       }
 
     function compileTextFn(el, attrs, transclude)
@@ -36,6 +59,8 @@
         templateObj.find('div.pseudotransclude').append(children);
 
         templateObj.find('button.drop-item-button').attr('ng-click', getDeleteFunction(attrs.parentObject, attrs.collectionName));
+
+        templateObj.find('button.push-collection-simple-button').attr('ng-click', getAddFunction(attrs.parentObject, attrs.collectionName));
 
         el.html('').append(templateObj);
       }
