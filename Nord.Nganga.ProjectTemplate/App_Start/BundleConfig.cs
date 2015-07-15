5e947490354e2abc0542d5bd7e8e8085407eced5
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-07-15 08:48:32 -0400

    bundle config for tempalte

--- Nord.Nganga.ProjectTemplate/App_Start/BundleConfig.cs
+++ Nord.Nganga.ProjectTemplate/App_Start/BundleConfig.cs
@@ -14,11 +14,12 @@ namespace Nord.Nganga.ProjectTemplate
 #else
         BundleTable.EnableOptimizations = true;
 #endif
+      
 
       var jquery = new ScriptBundle("~/bundles/jquery")
-        .Include("~/Scripts/libraries/jquery-2.1.1.js")
-        .Include("~/Scripts/libraries/modernizr-2.6.2.js")
-        .Include("~/Scripts/libraries/Blob.js");
+        .Include("~/client/lib/jq/jquery-2.1.4.js");
+        //.Include("~/Scripts/libraries/modernizr-2.6.2.js")
+        //.Include("~/Scripts/libraries/Blob.js");
 #if DEBUG
       jquery.Transforms.Clear();
 #endif
@@ -26,48 +27,48 @@ namespace Nord.Nganga.ProjectTemplate
 
       var bootStrapJs = new ScriptBundle("~/bundles/bootstrap")
         .Include(
-          "~/Scripts/libraries/bootstrap.js",
-          "~/Scripts/libraries/respond.js");
+          "~/client/lib/bootstrap/js/bootstrap.js");
+          //"~/Scripts/libraries/respond.js");
 
 #if DEBUG
       bootStrapJs.Transforms.Clear();
 #endif
 
       bundles.Add(bootStrapJs);
-
+      
       bundles.Add(new StyleBundle("~/Content/referrerPortalCSS")
         .Include(
-          "~/Content/bootstrap.css",
-          "~/Content/ng-grid.min.css",
-          "~/Content/site.css",
-          "~/Content/tablesort.css",
-          "~/Content/toaster.css"));
+          "~/client/lib/bootstrap/css/bootstrap.css",
+          //"~/Content/ng-grid.min.css",
+          "~/Client/lib/ng/plugin/nganga/css/Site.csss",
+          "~/Client/lib/ng/plugin/tablesort/tablesort.css",
+          "~/Client/lib/ng/plugin/toaster/toaster.css"));
 
       var ngBundle = new ScriptBundle("~/bundles/angular")
         .Include(
-          "~/Scripts/libraries/stacktrace.js",
-          "~/Scripts/libraries/spin.js",
-          "~/Scripts/libraries/lodash.js",
-          "~/Scripts/libraries/angular.js",
-          "~/Scripts/libraries/angular-animate.js",
-          "~/Scripts/libraries/angular-cookies.js",
-          "~/Scripts/libraries/angular-loader.js",
-          "~/Scripts/libraries/angular-resource.js",
-          "~/Scripts/libraries/angular-sanitize.js",
-          "~/Scripts/libraries/angular-touch.js",
-          "~/Scripts/libraries/angular-ui/ui-utils.js",
-          "~/Scripts/libraries/angular-ui/ui-bootstrap.js",
-          "~/Scripts/libraries/angular-ui-router.js",
-          "~/Scripts/libraries/angular-ui/ui-bootstrap-tpls.js",
-          "~/Scripts/libraries/ng-grid.js",
-          "~/Scripts/libraries/angular-tablesort.js",
-          "~/Scripts/libraries/ng-file-upload.js",
-          "~/Scripts/libraries/toaster.js",
-          "~/Scripts/referrerportal.js",
-          "~/Scripts/referrerPortalExceptionHandling.js",
-          "~/Scripts/ui/*.js")
-        .IncludeDirectory("~/Scripts/services/", "*.js", true)
-        .IncludeDirectory("~/Scripts/controllers/", "*.js", true);
+          "~/client/lib/stacktrace.js",
+          "~/client/lib/spin.js",
+          "~/client/lib/lodash.js",
+          "~/client/lib/ng/core/angular.js",
+          "~/client/lib/ng/core/angular-animate.js",
+          "~/client/lib/ng/core/angular-cookies.js",
+          "~/client/lib/ng/core/angular-loader.js",
+          "~/client/lib/ng/core/angular-resource.js",
+          "~/client/lib/ng/core/angular-sanitize.js",
+          "~/client/lib/ng/core/angular-touch.js",
+          "~/client/lib/ng/plugin/hg-ui/ui-utils.js",
+          "~/client/lib/ng/plugin/hg-ui/ui-bootstrap.js",
+          "~/client/lib/ng/plugin/hg-ui/angular-ui-router.js",
+          "~/client/lib/ng/plugin/hg-ui/ui-bootstrap-tpls.js",
+          //"~/Scripts/libraries/ng-grid.js",
+          "~/client/lib/ng/plugin/tablesort/angular-tablesort.js",
+         // "~/Scripts/libraries/ng-file-upload.js",
+          "~~/client/lib/ng/plugin/toaster/toaster.js",
+          "~/client/lib/ng/plugin/nganga/js/_ngangaModuleDeclaration.js",
+          "~/client/lib/ng/plugin/nganga/js/exceptionHandling.js")
+          //"~/Scripts/ui/*.js")
+        .IncludeDirectory("~/client/app/js/", "*.js", true);
+        //.IncludeDirectory("~/Scripts/controllers/", "*.js", true);
 
 #if DEBUG
       ngBundle.Transforms.Clear();
