c748d9da821786df9b3bf29d830460c5727227f1
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-07-15 11:53:53 -0400

    adding assy level annotation attributes

---
+++ Nord.Nganga.Annotations/Attributes/Html/TextCasePreferencesAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes.Html
+{
+  [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+  public class TextCasePreferencesAttribute : System.Attribute
+  {
+    public enum CasingOption
+    {
+      Sentence,
+      Title
+    }
+
+    public enum CasingOptionContext
+    {
+      Button,
+      Field,
+      Header,
+    }
+
+    public CasingOption Option { get; set; }
+    public CasingOptionContext Context { get; set; }
+
+    public TextCasePreferencesAttribute(CasingOptionContext context, CasingOption option)
+    {
+      this.Context = context;
+      this.Option = option;
+    }
+  }
+}
+
---
+++ Nord.Nganga.Annotations/Attributes/ProjectStructureAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes
+{
+  [AttributeUsage(AttributeTargets.Assembly)]
+  public class ProjectStructureAttribute:System.Attribute
+  {
+    public ProjectStructureAttribute(string ngControllersPath, string ngViewsPath, string ngResourcesPath, string csProjectPath)
+    {
+      this.NgControllersPath = ngControllersPath;
+      this.NgViewsPath = ngViewsPath;
+      this.NgResourcesPath = ngResourcesPath;
+      this.CsProjectPath = csProjectPath;
+    }
+
+    public string NgControllersPath { get; set; }
+    public string NgViewsPath { get; set; }
+    public string NgResourcesPath { get;set; }
+    public string CsProjectPath { get; set; }
+  }
+}
