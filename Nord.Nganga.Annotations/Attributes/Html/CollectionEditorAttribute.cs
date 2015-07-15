3d6bd503fa35840e3010a7495cae57c6334450d1
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2015-07-15 12:16:53 -0400

    Attributes for HTML gen.

---
+++ Nord.Nganga.Annotations/Attributes/Html/CollectionEditorAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes.Html
+{
+  /// <summary>
+  /// Controls the type of HTML editor to be used for a collection of complex objects.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property)]
+  public class CollectionEditorAttribute
+  {
+    /// <summary>
+    /// Create a new CollectionEditorAttribute.
+    /// </summary>
+    /// <param name="editor">The type of editor to be used.</param>
+    public CollectionEditorAttribute(EditorType editor)
+    {
+      this.Editor = editor;
+    }
+
+    /// <summary>
+    /// Type of editor control to use.
+    /// </summary>
+    public enum EditorType
+    {
+      /// <summary>
+      /// The simple editor, which displays all fields of all members of the collection
+      /// </summary>
+      Simple,
+
+      /// <summary>
+      /// The complex editor, which uses a table to show all primitive fields of all members and a form showing a single, selected member for editing
+      /// </summary>
+      Complex,
+    }
+
+    /// <summary>
+    /// The editor to be used.
+    /// </summary>
+    public EditorType Editor { get; private set; }
+  }
+}
---
+++ Nord.Nganga.Annotations/Attributes/Html/FieldWidthAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes.Html
+{
+  /// <summary>
+  /// Controls the width in Bootstrap columns of a field within HTML rows.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property)]
+  public class FieldWidthAttribute : Attribute
+  {
+    /// <summary>
+    /// Twelfths of the row to be used.
+    /// </summary>
+    public int Twelfths { get; private set; }
+
+    /// <summary>
+    /// Creates a new FieldWidthAttribute.
+    /// </summary>
+    /// <param name="twelfths">The number of Bootstrap columns the field is to occupy</param>
+    public FieldWidthAttribute(int twelfths)
+    {
+      if (twelfths < 1 || twelfths > 12)
+      {
+        throw new ArgumentOutOfRangeException("twelfths", "Twelfths must be between one and twelve.");
+      }
+      this.Twelfths = twelfths;
+    }
+  }
+}
