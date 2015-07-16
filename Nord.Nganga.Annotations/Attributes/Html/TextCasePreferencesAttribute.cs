a2940156d1ffd1af70775db528935a835a5b6536
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-07-16 15:11:47 -0400

    assembly options models work

--- Nord.Nganga.Annotations/Attributes/Html/TextCasePreferencesAttribute.cs
+++ Nord.Nganga.Annotations/Attributes/Html/TextCasePreferencesAttribute.cs
@@ -5,18 +5,6 @@ namespace Nord.Nganga.Annotations.Attributes.Html
   [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
   public class TextCasePreferencesAttribute : System.Attribute
   {
-    public enum CasingOption
-    {
-      Sentence,
-      Title
-    }
-
-    public enum CasingOptionContext
-    {
-      Button,
-      Field,
-      Header,
-    }
 
     public CasingOption Option { get; set; }
     public CasingOptionContext Context { get; set; }
---
+++ Nord.Nganga.ViewModels/AssemblyOptionsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nord.Nganga.Annotations;
+using Nord.Nganga.Annotations.Attributes;
+using Nord.Nganga.Annotations.Attributes.Html;
+
+namespace Nord.Nganga.Models
+{
+  public class AssemblyOptionsModel
+  {
+    private readonly Dictionary<CasingOptionContext, CasingOption> textCaseOptions;
+
+    public string NgControllersPath { get; set; }
+    public string NgViewsPath { get; set; }
+    public string NgResourcesPath { get; set; }
+    public string CsProjectPath { get; set; }
+
+    public CasingOption GetOption(CasingOptionContext context)
+    {
+      return this.textCaseOptions.ContainsKey(context) ? this.textCaseOptions[context] : this.textCaseOptions[CasingOptionContext.Default];
+    }
+
+    public AssemblyOptionsModel(Type type)
+    {
+      this.textCaseOptions = new Dictionary<CasingOptionContext, CasingOption>
+      {
+          {CasingOptionContext.Default,CasingOption.Sentence}
+      };
+
+      var tcpaList = type.GetCustomAttributes(typeof (TextCasePreferencesAttribute), true);
+
+      if (tcpaList.Any())
+      {
+        foreach (TextCasePreferencesAttribute tcpa in tcpaList)
+        {
+          this.textCaseOptions[tcpa.Context] = tcpa.Option;
+        }
+      }
+
+      var psa = type.GetCustomAttributes(typeof(ProjectStructureAttribute), true).Select(p=> (ProjectStructureAttribute)p).FirstOrDefault();
+      if (psa == null) return;
+      this.NgControllersPath = psa.NgControllersPath;
+      this.NgViewsPath = psa.NgViewsPath;
+      this.NgResourcesPath = psa.NgResourcesPath;
+      this.CsProjectPath = psa.CsProjectPath;
+    }
+  }
+}
