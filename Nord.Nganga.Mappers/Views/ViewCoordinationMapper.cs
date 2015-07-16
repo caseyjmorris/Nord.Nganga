ed7a77658c9598f11c15ff17e2cbd2626cdbaaee
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2015-07-16 15:31:55 -0400

    Coordination mapper (single)
    Still need to examine controller to pull out relevant types

--- Nord.Nganga.Mappers/Views/ViewCoordinationMapper.cs
+++ Nord.Nganga.Mappers/Views/ViewCoordinationMapper.cs
@@ -1,12 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Nord.Nganga.Core.Reflection;
+using Nord.Nganga.Core.Text;
+using Nord.Nganga.Models.ViewModels;
 
 namespace Nord.Nganga.Mappers.Views
 {
   public class ViewCoordinationMapper
   {
+    private readonly ViewModelMapper viewModelMapper;
+
+    public ViewCoordinationMapper(ViewModelMapper viewModelMapper)
+    {
+      this.viewModelMapper = viewModelMapper;
+    }
+
+    public IEnumerable<ViewCoordinatedInformationViewModel> GetViewCoordinatedInformationCollection(Type controller)
+    {
+      throw new NotImplementedException();
+    }
+
+    public ViewCoordinatedInformationViewModel GetViewCoordinatedInformationSingle(Type vmType)
+    {
+      var vmVm = this.viewModelMapper.GetViewModelViewModel(vmType);
+
+      var coord = new ViewCoordinatedInformationViewModel
+      {
+        //TODO:  case pref
+        SaveButtonText = "Save changes to " + vmType.GetFriendlyName().ToSpaced().ToLower(),
+        Sections = this.SplitSections(vmVm),
+      };
+
+      return coord;
+    }
+
+    private IEnumerable<ViewCoordinatedInformationViewModel.SectionViewModel> SplitSections(ViewModelViewModel vmVm)
+    {
+      var sections = new List<ViewCoordinatedInformationViewModel.SectionViewModel>();
+
+      const int rowMax = 12;
+
+      var currentSection = new ViewCoordinatedInformationViewModel.SectionViewModel
+      {
+        Title = vmVm.Members[0].Section,
+        Rows = new List<ViewCoordinatedInformationViewModel.RowViewModel>()
+      };
+
+      sections.Add(currentSection);
+
+      var currentRow = new ViewCoordinatedInformationViewModel.RowViewModel
+      {
+        Members = new List<ViewModelViewModel.MemberWrapper>(),
+      };
+
+      currentSection.Rows.Add(currentRow);
+
+      var currentRowWidth = 0;
+
+      foreach (var member in vmVm.Members)
+      {
+        if (member.Section != currentSection.Title)
+        {
+          currentSection = new ViewCoordinatedInformationViewModel.SectionViewModel
+          {
+            Title = member.Section,
+            Rows = new List<ViewCoordinatedInformationViewModel.RowViewModel>(),
+          };
+
+          currentRow = new ViewCoordinatedInformationViewModel.RowViewModel
+          {
+            Members = new List<ViewModelViewModel.MemberWrapper>(),
+          };
+
+          currentSection.Rows.Add(currentRow);
+
+          currentRowWidth = 0;
+        }
+
+        currentRowWidth += member.Width;
+
+        if (currentRowWidth > rowMax)
+        {
+          currentRow = new ViewCoordinatedInformationViewModel.RowViewModel
+          {
+            Members = new List<ViewModelViewModel.MemberWrapper>(),
+          };
+
+          currentSection.Rows.Add(currentRow);
+
+          currentRowWidth = member.Width;
+        }
+
+        currentRow.Members.Add(member);
+      }
+
+      return sections;
+    }
   }
 }
--- Nord.Nganga.ViewModels/ViewModels/ViewCoordinatedInformationViewModel.cs
+++ Nord.Nganga.ViewModels/ViewModels/ViewCoordinatedInformationViewModel.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Nord.Nganga.Models.ViewModels
 {
   public class ViewCoordinatedInformationViewModel
   {
+    public IEnumerable<SectionViewModel> Sections { get; set; }
+
+    public string SaveButtonText { get; set; }
+
     public class RowViewModel
     {
-      
+      public ICollection<ViewModelViewModel.MemberWrapper> Members { get; set; }
     }
 
-    public class RowMemberViewModel
+    public class SectionViewModel
     {
-      
+      public string Title { get; set; }
+      public ICollection<RowViewModel> Rows { get; set; }
     }
   }
 }
