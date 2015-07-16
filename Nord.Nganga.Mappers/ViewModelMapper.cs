be059671a2a94f6dfb94949d5647d41f39efa13f
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-07-16 13:29:28 -0400

    view model mapping

--- Nord.Nganga.Mappers/ViewModelMapper.cs
+++ Nord.Nganga.Mappers/ViewModelMapper.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Reflection;
@@ -50,11 +51,88 @@ namespace Nord.Nganga.Mappers
           && !complexCollectionDetector(info);
 
         // dictionary mapping property detectors to member descriminator enum value 
-        var fd = new Dictionary<Func<PropertyInfo, bool>, ViewModelViewModel.MemberDiscriminator>
+        var discriminatorProviderDictionary = new Dictionary<Func<PropertyInfo, bool>, ViewModelViewModel.MemberDiscriminator>
         {
           { scalarDetector, ViewModelViewModel.MemberDiscriminator.Scalar},
           { primitiveCollectionDetector, ViewModelViewModel.MemberDiscriminator.PrimitiveCollection},
           { complexCollectionDetector, ViewModelViewModel.MemberDiscriminator.ComplexCollection}
+        };
+
+      Func<PropertyInfo, ViewModelViewModel.FieldViewModel> fieldViewModelProvider = info =>
+      {
+        var isSelectCommon = info.HasAttribute<SelectCommonAttribute>();
+        var selectCommonAttribute = isSelectCommon ? info.GetAttribute<SelectCommonAttribute>() : null;
+        var commonRecordsName = isSelectCommon ? selectCommonAttribute.CommonInformationName.ToCamelCase() : null;
+
+        var fieldModel = new ViewModelViewModel.FieldViewModel
+        {
+          DataType = info.PropertyType,
+          DisplayName =
+            info.HasAttribute<DisplayAttribute>()
+              ? info.GetAttribute<DisplayAttribute>().Name
+              : info.Name.ToSpaced() + (info.PropertyType.GetNonNullableType() == typeof(bool) ? "?" : String.Empty),
+          FieldName = info.Name.ToCamelCase(),
+          IsHidden = info.HasAttribute<DoNotShowAttribute>(),
+          IsRequired = info.HasAttribute<RequiredAttribute>(),
+          IsViewOnly = info.HasAttribute<NotUserEditableAttribute>(),
+          Section =
+            info.GetAttributePropertyValueOrDefault<UiSectionAttribute, string>(a => a.SectionName) ?? String.Empty,
+          SelectCommon = selectCommonAttribute,
+          IsDefaultSort = info.HasAttribute<DefaultSortAttribute>(),
+          InputMask = info.HasAttribute<InputMaskAttribute>() ? info.GetAttribute<InputMaskAttribute>().Mask : null,
+          Minimum = info.HasAttribute<RangeAttribute>() ? info.GetAttribute<RangeAttribute>().Minimum : null,
+          Maximum = info.HasAttribute<RangeAttribute>() ? info.GetAttribute<RangeAttribute>().Maximum : null,
+        };
+
+        return fieldModel;
+      };
+
+      Func<IEnumerable<PropertyInfo>, IEnumerable<ViewModelViewModel.FieldViewModel>> fieldViewModelCollectionProvider = t =>
+      {
+        var pfi = t.Select(s => fieldViewModelProvider(s));
+        return pfi;
+      };
+
+
+      Func<PropertyInfo, ViewModelViewModel.SubordinateViewModelWrapper> subordinateViewModelWrapperProvider = (info) =>
+      {
+        var hasEnumerableItemAction = info.HasAttribute<SubordinateItemActionAttribute>();
+        var itemActionAttribute = hasEnumerableItemAction
+          ? info.GetAttribute<SubordinateItemActionAttribute>()
+          : null;
+        var wrapper = new ViewModelViewModel.SubordinateViewModelWrapper
+        {
+          Model =
+            this.GetViewModelViewModel(info.PropertyType.GetGenericArguments().First()),
+          Section =
+            info.GetAttributePropertyValueOrDefault<UiSectionAttribute, string>(s => s.SectionName) ?? string.Empty,
+          DisplayName =
+            info.GetAttributePropertyValueOrDefault<DisplayAttribute, string>(a => a.Name) ?? info.Name.ToSpaced(),
+          IsLedger = info.HasAttribute<LedgerAttribute>(),
+          LedgerSumProperty =
+            info.GetAttributePropertyValueOrDefault<LedgerAttribute, string>(a => a.SumPropertyName),
+          ItemActionAttribute = itemActionAttribute,
+        };
+        return wrapper;
+      };
+
+      Func<ViewModelViewModel.MemberDiscriminator, PropertyInfo, ViewModelViewModel.MemberWrapper> memberWrapperProvider =
+        (discriminator, info) =>
+        {
+          var result = new ViewModelViewModel.MemberWrapper{Discriminator = discriminator};
+          switch (discriminator)
+          {
+            case ViewModelViewModel.MemberDiscriminator.Scalar:
+              result.Member = fieldViewModelProvider(info);
+              break;
+            case ViewModelViewModel.MemberDiscriminator.PrimitiveCollection:
+              result.Member = subordinateViewModelWrapperProvider(info);
+              break;
+            case ViewModelViewModel.MemberDiscriminator.ComplexCollection:
+              result.Member = subordinateViewModelWrapperProvider(info);
+              break;
+          }
+          return result;
         };
 
         // get the properties 
@@ -62,8 +140,9 @@ namespace Nord.Nganga.Mappers
 
       // decorate them with the descriminator 
       var decoratedProperties = (from PropertyInfo pi in viewModelProperties
-        let descriminator = fd.Single(kvp => kvp.Key(pi)).Value
-                                select new { pi, descriminator }).ToList();
+        let discriminator = discriminatorProviderDictionary.Single(kvp => kvp.Key(pi)).Value
+        let memberWrapper = memberWrapperProvider(discriminator, pi )
+                                select new { pi, discriminator,  memberWrapper }).ToList();
 
                                 // VVVVVV OLD CODE VVVVVV
 
@@ -75,70 +154,19 @@ namespace Nord.Nganga.Mappers
       //  type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
       //    .Where(t => !typeof(IEnumerable).IsAssignableFrom(t.PropertyType) || t.PropertyType == typeof(string));
 
-      Func<IEnumerable<PropertyInfo>, IEnumerable<ViewModelViewModel.FieldViewModel>> getPrimitiveFieldInfo = t =>
-      {
-        var pfi = t.Select(s =>
-        {
-          var isSelectCommon = s.HasAttribute<SelectCommonAttribute>();
-          var selectCommonAttribute = isSelectCommon ? s.GetAttribute<SelectCommonAttribute>() : null;
-          var commonRecordsName = isSelectCommon ? selectCommonAttribute.CommonInformationName.ToCamelCase() : null;
 
-          var fieldModel = new ViewModelViewModel.FieldViewModel
-          {
-            DataType = s.PropertyType,
-            DisplayName =
-              s.HasAttribute<DisplayAttribute>()
-                ? s.GetAttribute<DisplayAttribute>().Name
-                : s.Name.ToSpaced() + (s.PropertyType.GetNonNullableType() == typeof(bool) ? "?" : String.Empty),
-            FieldName = s.Name.ToCamelCase(),
-            IsHidden = s.HasAttribute<DoNotShowAttribute>(),
-            IsRequired = s.HasAttribute<RequiredAttribute>(),
-            IsViewOnly = s.HasAttribute<NotUserEditableAttribute>(),
-            Section =
-              s.GetAttributePropertyValueOrDefault<UiSectionAttribute, string>(a => a.SectionName) ?? String.Empty,
-            SelectCommon = selectCommonAttribute,
-            IsDefaultSort = s.HasAttribute<DefaultSortAttribute>(),
-            InputMask = s.HasAttribute<InputMaskAttribute>() ? s.GetAttribute<InputMaskAttribute>().Mask : null,
-            Minimum = s.HasAttribute<RangeAttribute>() ? s.GetAttribute<RangeAttribute>().Minimum : null,
-            Maximum = s.HasAttribute<RangeAttribute>() ? s.GetAttribute<RangeAttribute>().Maximum : null,
-          };
-
-          return fieldModel;
-        });
-
-        return pfi;
-      };
 
       var vm = new ViewModelViewModel
       {
         Name = type.Name.Replace("ViewModel", string.Empty).ToCamelCase(),
-        Scalars = getPrimitiveFieldInfo(decoratedProperties.Where(p=>p.descriminator == ViewModelViewModel.MemberDiscriminator.Scalar).Select(p=>p.pi)),
+        Scalars = fieldViewModelCollectionProvider(decoratedProperties.Where(p => p.discriminator == ViewModelViewModel.MemberDiscriminator.Scalar).Select(p => p.pi)),
         IsViewOnly = type.HasAttribute<NotUserEditableAttribute>(),
         ComplexCollections =
-          decoratedProperties.Where(p => p.descriminator == ViewModelViewModel.MemberDiscriminator.ComplexCollection).Select(p=>p.pi)
-            .Select(t =>
-            {
-              var hasEnumerableItemAction = t.HasAttribute<SubordinateItemActionAttribute>();
-              var itemActionAttribute = hasEnumerableItemAction
-                ? t.GetAttribute<SubordinateItemActionAttribute>()
-                : null;
-              var wrapper = new ViewModelViewModel.SubordinateViewModelWrapper
-              {
-                Model =
-                  this.GetViewModelViewModel(t.PropertyType.GetGenericArguments().First()),
-                Section =
-                  t.GetAttributePropertyValueOrDefault<UiSectionAttribute, string>(s => s.SectionName) ?? string.Empty,
-                DisplayName =
-                  t.GetAttributePropertyValueOrDefault<DisplayAttribute, string>(a => a.Name) ?? t.Name.ToSpaced(),
-                IsLedger = t.HasAttribute<LedgerAttribute>(),
-                LedgerSumProperty =
-                  t.GetAttributePropertyValueOrDefault<LedgerAttribute, string>(a => a.SumPropertyName),
-                ItemActionAttribute = itemActionAttribute,
-              };
-              return wrapper;
-            }),
+          decoratedProperties.Where(p => p.discriminator == ViewModelViewModel.MemberDiscriminator.ComplexCollection).Select(p=>p.pi)
+            .Select(t => subordinateViewModelWrapperProvider(t)),
         PrimitiveCollections =
-          getPrimitiveFieldInfo(decoratedProperties.Where(p => p.descriminator == ViewModelViewModel.MemberDiscriminator.PrimitiveCollection).Select(p => p.pi)),
+          fieldViewModelCollectionProvider(decoratedProperties.Where(p => p.discriminator == ViewModelViewModel.MemberDiscriminator.PrimitiveCollection).Select(p => p.pi)),
+        Members = decoratedProperties.Select(dp => dp.memberWrapper).ToList()
       };
 
       var sectionsDict = new Dictionary<string, Dictionary<string, string>>();
@@ -157,6 +185,8 @@ namespace Nord.Nganga.Mappers
       }
 
       vm.SectionHtmlAttributeValues = sectionsDict;
+
+      
 
       return vm;
     }
