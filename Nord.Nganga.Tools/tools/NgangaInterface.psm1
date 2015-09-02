8e6fabfd0f5cabff2e80363643db80b2da3edfc9
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-09-02 10:58:45 -0400

    added documentation annotations added createDirectory redirected operation output to null to eliminate unwanted console output on integration operation

--- Nord.Nganga.Tools/tools/NgangaInterface.psm1
+++ Nord.Nganga.Tools/tools/NgangaInterface.psm1
@@ -5,11 +5,28 @@
   Add-Type -Path "$($PSScriptRoot)\bin\Nord.Nganga.Fs.dll"  
 }
 
+<#
+    .SYNOPSIS 
+        Returns the nganga settings types.
+
+    .DESCRIPTION
+        Returns the nganga settings types.
+#>
 Function Get-NgangaSettingsTypes
 {
   return [Nord.Nganga.Commands.Commands]::ListOptionTypes()
 }
 
+<#
+    .SYNOPSIS 
+        Returns the named nganga setting object.
+
+    .DESCRIPTION
+        Returns the named nganga setting object.
+
+    .PARAMETER SettingName          
+        The name of the setting to return.
+#>
 Function Get-NgangaSettings
 {
     param (
@@ -21,6 +38,16 @@ Function Get-NgangaSettings
     return [Nord.Nganga.Commands.Commands]::GetOptions($SettingName)
 }
 
+<#
+    .SYNOPSIS 
+        Updates nganga settings with the specified.
+
+    .DESCRIPTION
+        Updates nganga settings with the specified.
+
+    .PARAMETER OptionsObject          
+        The new settings object.
+#>
 Function Update-NgangaSettings
 {
   param(
@@ -37,6 +64,8 @@ Function Build-ControllerType
   $proj = Get-Project
 
   $cfg = $dte.Solution.SolutionBuild.ActiveConfiguration.Name
+
+  Write-Host "Building " $proj.UniqueName ", please wait..."
 
   $dte.Solution.SolutionBuild.BuildProject($cfg, $proj.UniqueName, $true)
 
@@ -59,6 +88,23 @@ Function Get-AssemblyFileName
   return [System.IO.Path]::Combine($Directory, $OutputPath, $AsmName ) + ".dll"
 }
 
+<#
+    .SYNOPSIS 
+        Lists type names of eligible controllers.
+
+    .DESCRIPTION
+        Reflects over the project assembly searching for controller implementations annotated with Nganga annotations.  
+
+    .PARAMETER Filter 
+        Scopes the search to type names containing the filter string.
+
+    .PARAMETER ResourceOnly
+        Scopes the search - ??? is this parameter actually used on this command? 
+
+    .PARAMETER Echo
+        Enables processing feedback to the host console.
+        
+#>
 Function Get-NgangaEligibleControllers
 {
   param(
@@ -86,6 +132,41 @@ Function Get-NgangaEligibleControllers
   return $result 
 }
 
+<#
+    .SYNOPSIS 
+        Generates for the named controller.
+
+    .DESCRIPTION
+        Generates the Angular UI resource, controller and view for the named controller.                    
+
+    .PARAMETER ResourceOnly
+        Limits the generation to the Angular resource file only.  No angular view or controller is generated. 
+
+    .PARAMETER Echo
+        Enables processing feedback to the host console.
+
+    .PARAMETER Preview
+        Supresses disk writes.  Dumps the generated source to the console for re/view. 
+
+    .PARAMETER Force
+        The generation step checks for an existing file on disk with the target file name.  If a file is found on disk
+        the file is opened and parsed.  If a valid Nganga header is found the header is parsed for the original md5.  
+        a new md5 is calculated for the body and compared with the md5 declared on the header.  
+        
+        If no header can be found or a header parse fails, or a parsed md5 differs from a calculated md5, 
+        then the newly generated source will not be written to disk without using -Force.  
+        
+        If a change is suspected and -Force is not specified, then a -Diff will
+        automatically be performed.  Refer to the discussion of Diff for more information on the diff.
+
+    .PARAMETER Diff
+        Invokes a diff on the newly generated source against the previous disk version.  If diff is specified
+        no changes are written to disk.  Diff may be invoked automatically if changes are detected or suspected 
+        in a previous version and force was not specified.
+
+    .PARAMETER Merge
+        Invokes the external merge tool identified by Get-NgangaDiffMergeToolPath.        
+#>
 Function Export-NgangaCode
 {
     param(
@@ -261,39 +342,43 @@ function Update-Project([string]$source, [string]$fileName, [bool]$noSaveRequire
     else {        
         try {
             
-            Write-Host "Writing file: " $fileName
-            $source | Out-File $fileName -encoding Unicode
+            Export-SourceFile $source $fileName 
 
             try {
                 $proj = Get-Project  
                 Write-Host "Integrating file: " $fileName 
-                #$dte.Application.ItemOperations.AddExistingItem($relativeFileName) 
-                #$DTE.ExecuteCommand(“Project.AddExistingItem”, $fileName)
-                $proj.ProjectItems.AddFromFile( $fileName )
+                #$dte.Application.ItemOperations.AddExistingItem($relativeFileName)  | out-null
+                #$DTE.ExecuteCommand(“Project.AddExistingItem”, $fileName) | out-null
+                $proj.ProjectItems.AddFromFile( $fileName ) | out-null
 
+                try {
+                  #Write-Host "Adding file to source control: " $fileName                
+                  #$DTE.ExecuteCommand(“File.AddToSourceControl”, $fileName) | out-null
+
+                  try {
+                    $DTE.ExecuteCommand(“File.OpenFile”, $fileName) | out-null
+                  }
+                  catch [System.Exception]{
+                    Write-Host $_
+                  }
+                }
+                catch [System.Exception]{
+                  Write-Host $_
+              } 
             }
             catch [System.Exception]{
                 Write-Host $_
             } 
-
-            try {
-                #Write-Host "Adding file to source control: " $fileName                
-                #$DTE.ExecuteCommand(“File.AddToSourceControl”, $fileName)
-            }
-            catch [System.Exception]{
-                Write-Host $_
-            } 
-
-            try {
-                $DTE.ExecuteCommand(“File.OpenFile”, $fileName)
-            }
-            catch [System.Exception]{
-                Write-Host $_
-            }
-
         }
         catch{}
     }
+}
+
+function Export-SourceFile([string] $source, [string]$fileName){
+    $dir = [System.IO.Path]::GetDirectoryName($fileName)
+    System.IO.Directory.CreateDirectory( $dir ) 
+    Write-Host "Writing file: " $fileName
+    $source | Out-File $fileName -encoding Unicode
 }
 
 New-Alias nng-gen Export-NgangaCode
