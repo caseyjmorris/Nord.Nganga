d402495284397df0a9567a8cb3948ba550abc511
psllc <mark.lloyd@practicalSoftwareLLC.com>
2015-10-05 18:39:42 -0400

    added support for view-only on nord-date fields

--- Nord.Nganga.TestConsumer/Models/SponsorProgramPeriodDetailCollectionViewModel.cs
+++ Nord.Nganga.TestConsumer/Models/SponsorProgramPeriodDetailCollectionViewModel.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Nord.Nganga.Annotations.Attributes.Html;
+using Nord.Nganga.Annotations.Attributes.ViewModels;
 
 namespace Nord.Nganga.TestConsumer.Models
 {
+  [NotUserEditable]
   public class SponsorProgramPeriodDetailCollectionViewModel : ProviderProgramPeriodDetailCollectionBaseViewModel
   {
     [CollectionEditor(CollectionEditorAttribute.EditorType.Complex)]
     public IEnumerable<SponsorViewModel> Sponsors { get; set; }
+
+    public DateTime SomeDate { get; set; }
   }
 }
