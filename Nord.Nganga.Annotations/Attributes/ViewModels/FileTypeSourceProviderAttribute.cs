5f98f57b728566a5124ae7bdabb0ca59a7d4646d
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2015-12-01 14:44:53 -0500

    GEtting started...

---
+++ Nord.Nganga.Annotations/Attributes/ViewModels/FileTypeSourceProviderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes.ViewModels
+{
+  /// <summary>
+  /// When provided on collection of <see cref="Nganga.DataTypes.UserFileViewModel"/> objects, defines the expression used to supply file types.  If blank this field will not be shown.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Property)]
+  public class FileTypeSourceProviderAttribute : Attribute
+  {
+    /// <summary>
+    /// Body of expression to be used to retrieve document types
+    /// </summary>
+    public string Expression { get; private set; }
+
+    /// <summary>
+    /// Instantiate a new <see cref="FileTypeSourceProviderAttribute"/>
+    /// </summary>
+    /// <param name="expression">Body of expression to be used to retrieve document types</param>
+    public FileTypeSourceProviderAttribute(string expression)
+    {
+      this.Expression = expression;
+    }
+  }
+}
