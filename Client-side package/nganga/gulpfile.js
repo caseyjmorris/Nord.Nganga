b3b71a12393c95490b90d23ab384ef7a891925ce
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2016-04-28 16:24:43 -0400

    Closer to having something that can be easily included

--- Client-side package/nganga/gulpfile.js
+++ Client-side package/nganga/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var templateCache = require('gulp-angular-templatecache');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -10,8 +13,13 @@ var BUILD = 'build';
 
 gulp.task('minify', function()
 {
-  gulp.src('js/**/*.js')
+  gulp.src(['js/nganga.js', 'js/interpolate.js', 'js/http.handling.js', 'js/exception.handling.js', 'js/svc/**/*.js', 'js/ui/**/*.js'])
+  .pipe(sourcemaps.init())
+  .pipe(concat('nganga.dist.js'))
+  .pipe(gulp.dest(BUILD))
+  .pipe(rename('nganga.dist.min.js'))
   .pipe(uglify())
+  .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest(BUILD))
 });
 
