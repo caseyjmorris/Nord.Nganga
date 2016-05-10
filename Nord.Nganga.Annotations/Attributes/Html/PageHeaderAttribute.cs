7d3f9cfa114ad021fcdff70a76d5ae425f24db46
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2016-05-10 16:32:37 -0400

    Add ability to override default page header.

---
+++ Nord.Nganga.Annotations/Attributes/Html/PageHeaderAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nord.Nganga.Annotations.Attributes.Html
+{
+  /// <summary>
+  /// If specified, override the default behavior of using the controller name as the header for the page.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Class)]
+  public class PageHeaderAttribute : Attribute
+  {
+    /// <summary>
+    /// Instantiate a new <see cref="PageHeaderAttribute"/>
+    /// </summary>
+    /// <param name="header">The text of the header</param>
+    public PageHeaderAttribute(string header)
+    {
+      this.Header = header;
+    }
+
+    /// <summary>
+    /// The text of the header
+    /// </summary>
+    public string Header { get; private set; }
+  }
+}
