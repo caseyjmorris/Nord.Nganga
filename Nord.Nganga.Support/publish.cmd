9953162521b997514c81528f8ef166ee2fac997e
psllc <mark.lloyd@practicalSoftwareLLC.com>
2016-05-11 08:58:30 -0400

    updates to publish support

--- Nord.Nganga.Support/publish.cmd
+++ Nord.Nganga.Support/publish.cmd
@@ -1,2 +1,2 @@
-nuget pack "C:\Users\mlloyd\Source\Repos\Nord.Nganga\Nord.Nganga.Support\Nord.Nganga.Support.1.0.5967.24575.nuspec"
-nuget push "C:\Users\mlloyd\Source\Repos\Nord.Nganga\Nord.Nganga.Support\Nord.Nganga.Support.1.0.5967.24575.nupkg"   -s https://www.myget.org/F/nord-pkg/ 
+nuget pack "C:\Users\mlloyd\Source\Repos\Nord.Nganga\Nord.Nganga.Support\Nord.Nganga.Support.1.0.5968.18205.nuspec"
+nuget push "C:\Users\mlloyd\Source\Repos\Nord.Nganga\Nord.Nganga.Support\Nord.Nganga.Support.1.0.5968.18205.nupkg"   -s https://www.myget.org/F/nord-pkg/ 
